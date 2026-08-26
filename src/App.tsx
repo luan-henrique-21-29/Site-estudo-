2fcd09e48ffc0d3e7a404dbcc88d2e6140c77232
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-26 16:39:18 -0300

    feat: add world map and PWA update notification

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { Onboarding } from './components/Onboarding'
 import { RouteTracker } from './components/RouteTracker'
+import { AppUpdateBanner } from './components/AppUpdateBanner'
 import { Home } from './pages/Home'
 import { CoursePage } from './pages/CoursePage'
 import { LessonPage } from './pages/LessonPage'
@@ -37,6 +38,7 @@ import { CountryGuidePage } from './pages/CountryGuidePage'
 import { ProgrammingRoadmapPage } from './pages/ProgrammingRoadmapPage'
 import { CodeChallengesPage } from './pages/CodeChallengesPage'
 import { PortfolioPage } from './pages/PortfolioPage'
+import { WorldMapPage } from './pages/WorldMapPage'
 
 const SalariesPage = lazy(() => import('./pages/SalariesPage').then(m => ({ default: m.SalariesPage })))
 const PlaygroundPage = lazy(() => import('./pages/PlaygroundPage').then(m => ({ default: m.PlaygroundPage })))
@@ -46,10 +48,10 @@ function Loading(){return <div className="page"><div className="empty glass-pane
 function NotFound(){return <div className="page"><div className="empty glass-panel"><h1>404</h1><p>Essa página resolveu fazer intercâmbio e ainda não voltou. 😅</p><a href="#/">Voltar ao início</a></div></div>}
 
 export default function App(){
- return <HashRouter><RouteTracker/><Onboarding/><Suspense fallback={<Loading/>}><Routes><Route element={<Layout/>}>
+ return <HashRouter><RouteTracker/><Onboarding/><AppUpdateBanner/><Suspense fallback={<Loading/>}><Routes><Route element={<Layout/>}>
   <Route index element={<Home/>}/><Route path="today" element={<StudyToday/>}/><Route path="focus" element={<FocusPage/>}/>
   <Route path="course/:course" element={<CoursePage/>}/><Route path="lesson/:id" element={<LessonPage/>}/><Route path="english-lab" element={<EnglishLabPage/>}/>
-  <Route path="countries" element={<CountriesPage/>}/><Route path="countries/:id" element={<CountryPage/>}/><Route path="country-guide/:id" element={<CountryGuidePage/>}/><Route path="compare-countries" element={<CountryComparePage/>}/>
+  <Route path="countries" element={<CountriesPage/>}/><Route path="countries/:id" element={<CountryPage/>}/><Route path="country-guide/:id" element={<CountryGuidePage/>}/><Route path="compare-countries" element={<CountryComparePage/>}/><Route path="world-map" element={<WorldMapPage/>}/>
   <Route path="cities" element={<CitiesPage/>}/><Route path="cities/:countryId/:citySlug" element={<CityDetailPage/>}/><Route path="compare-cities" element={<CityComparePage/>}/><Route path="salaries" element={<SalariesPage/>}/>
   <Route path="programming-roadmap" element={<ProgrammingRoadmapPage/>}/><Route path="code-challenges" element={<CodeChallengesPage/>}/><Route path="playground" element={<PlaygroundPage/>}/><Route path="portfolio" element={<PortfolioPage/>}/>
   <Route path="careers" element={<CareersPage/>}/><Route path="careers/:id" element={<CareerDetailPage/>}/><Route path="future" element={<FuturePage/>}/><Route path="finance" element={<FinancialPlannerPage/>}/>
