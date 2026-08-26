e92f671fa924f3a1a2b61ffdd51a9b9b84a11976
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-26 11:47:12 -0300

    feat: add English curriculum

---
+++ src/data/lessonTopics/english.ts
@@ -0,0 +1,80 @@
+export const englishTopics = `Starter|Starter|Alfabeto e sons básicos
+Starter|Starter|Números no dia a dia
+Starter|Starter|Horas e horários
+Starter|Starter|Datas, dias e meses
+Starter|Starter|Cores e descrições
+Starter|Starter|Família
+Starter|Starter|Casa e cômodos
+Starter|Starter|Comida e bebida
+Starter|Starter|Roupas
+Starter|Starter|Cidade e lugares
+Dia a dia|A1|Cumprimentos naturais
+Dia a dia|A1|Se apresentando
+Dia a dia|A1|Small talk
+Dia a dia|A1|Pedindo ajuda
+Dia a dia|A1|Pedindo informação
+Dia a dia|A1|No supermercado
+Dia a dia|A1|No restaurante
+Dia a dia|A1|No fast food
+Dia a dia|A1|No shopping
+Dia a dia|A1|No ônibus
+Dia a dia|A1|No trem
+Dia a dia|A1|No táxi
+Dia a dia|A1|No aeroporto
+Dia a dia|A1|No hotel
+Dia a dia|A1|Na rua
+Dia a dia|A1|No banco
+Dia a dia|A1|Na farmácia
+Dia a dia|A2|No hospital
+Dia a dia|A2|Em uma emergência
+Dia a dia|A2|Na academia
+Dia a dia|A2|Falando com amigos
+Dia a dia|A2|Relacionamentos
+Dia a dia|A2|Na escola
+Dia a dia|A2|Na faculdade
+Dia a dia|A2|Alugando uma casa
+Dia a dia|A2|Falando ao telefone
+Dia a dia|A2|Internet e suporte
+Dia a dia|A2|Falando com a polícia
+Dia a dia|A2|Na alfândega
+Dia a dia|A2|Pedindo reembolso
+Confusões|A2|Actually
+Confusões|A2|Eventually
+Confusões|A2|Pretend
+Confusões|A2|Realize
+Confusões|A2|Parents
+Confusões|A2|Library
+Confusões|A2|College
+Confusões|A2|Fabric
+Confusões|A2|Sensible
+Confusões|A2|Notice
+Comparações|A2|History × Story
+Comparações|A2|House × Home
+Comparações|A2|Job × Work
+Comparações|A2|Fun × Funny
+Comparações|A2|Speak × Talk
+Comparações|A2|Say × Tell
+Comparações|A2|Hear × Listen
+Comparações|A2|See × Look × Watch
+Comparações|A2|Learn × Teach
+Comparações|A2|Remember × Remind
+Comparações|B1|Borrow × Lend
+Comparações|B1|Make × Do
+Comparações|B1|Trip × Travel × Journey
+Comparações|B1|Miss × Lose
+Comparações|B1|Much × Many
+Trabalho|A2|Primeiro dia no trabalho
+Trabalho|B1|Entrevista de emprego
+Trabalho|B1|Currículo e experiência
+Trabalho|A2|Pedindo explicação no trabalho
+Trabalho|A2|Falando com colegas
+Trabalho|B1|Falando com o chefe
+Trabalho|B1|Horários, folgas e férias
+Trabalho|B1|E-mails profissionais
+Trabalho|B1|Reuniões
+Trabalho|B1|Problemas e feedback
+Gramática|A1|Verb to be
+Gramática|A1|Simple Present
+Gramática|A1|Present Continuous
+Gramática|A2|Simple Past
+Gramática|A2|Future with will and going to`
