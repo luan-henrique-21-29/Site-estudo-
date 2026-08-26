a83d023f149749e3b0ce3742c99f4c0354418efe
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-26 12:27:23 -0300

    style: load salary comparison styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import { AppStateProvider } from './hooks/useAppState'
 import './styles.css'
 import './activities.css'
+import './salaries.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><AppStateProvider><App/></AppStateProvider></StrictMode>)
 
