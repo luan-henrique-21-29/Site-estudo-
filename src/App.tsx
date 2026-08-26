6fa2b429cb9b23f11471b1f0fbf0e494b85d4a72
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-26 15:08:43 -0300

    feat: add account route and resume tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { Onboarding } from './components/Onboarding'
+import { RouteTracker } from './components/RouteTracker'
 import { Home } from './pages/Home'
 import { CoursePage } from './pages/CoursePage'
 import { LessonPage } from './pages/LessonPage'
@@ -22,9 +23,10 @@ import { ToolsPage } from './pages/ToolsPage'
 import { SettingsPage } from './pages/SettingsPage'
 import { FlashcardsPage } from './pages/FlashcardsPage'
 import { QuizzesPage } from './pages/QuizzesPage'
+import { AccountPage } from './pages/AccountPage'
 
 function NotFound(){return <div className="page"><div className="empty glass-panel"><h1>404</h1><p>Essa página resolveu fazer intercâmbio e ainda não voltou. 😅</p><a href="#/">Voltar ao início</a></div></div>}
 
 export default function App(){
- return <HashRouter><Onboarding/><Routes><Route element={<Layout/>}><Route index element={<Home/>}/><Route path="today" element={<StudyToday/>}/><Route path="focus" element={<FocusPage/>}/><Route path="course/:course" element={<CoursePage/>}/><Route path="lesson/:id" element={<LessonPage/>}/><Route path="countries" element={<CountriesPage/>}/><Route path="countries/:id" element={<CountryPage/>}/><Route path="cities" element={<CitiesPage/>}/><Route path="salaries" element={<SalariesPage/>}/><Route path="playground" element={<PlaygroundPage/>}/><Route path="careers" element={<CareersPage/>}/><Route path="future" element={<FuturePage/>}/><Route path="review" element={<ReviewPage/>}/><Route path="flashcards" element={<FlashcardsPage/>}/><Route path="quizzes" element={<QuizzesPage/>}/><Route path="notebook" element={<NotebookPage/>}/><Route path="favorites" element={<FavoritesPage/>}/><Route path="progress" element={<ProgressPage/>}/><Route path="tools" element={<ToolsPage/>}/><Route path="search" element={<SearchPage/>}/><Route path="settings" element={<SettingsPage/>}/><Route path="*" element={<NotFound/>}/></Route></Routes></HashRouter>
+ return <HashRouter><RouteTracker/><Onboarding/><Routes><Route element={<Layout/>}><Route index element={<Home/>}/><Route path="today" element={<StudyToday/>}/><Route path="focus" element={<FocusPage/>}/><Route path="course/:course" element={<CoursePage/>}/><Route path="lesson/:id" element={<LessonPage/>}/><Route path="countries" element={<CountriesPage/>}/><Route path="countries/:id" element={<CountryPage/>}/><Route path="cities" element={<CitiesPage/>}/><Route path="salaries" element={<SalariesPage/>}/><Route path="playground" element={<PlaygroundPage/>}/><Route path="careers" element={<CareersPage/>}/><Route path="future" element={<FuturePage/>}/><Route path="review" element={<ReviewPage/>}/><Route path="flashcards" element={<FlashcardsPage/>}/><Route path="quizzes" element={<QuizzesPage/>}/><Route path="notebook" element={<NotebookPage/>}/><Route path="favorites" element={<FavoritesPage/>}/><Route path="progress" element={<ProgressPage/>}/><Route path="tools" element={<ToolsPage/>}/><Route path="search" element={<SearchPage/>}/><Route path="settings" element={<SettingsPage/>}/><Route path="account" element={<AccountPage/>}/><Route path="*" element={<NotFound/>}/></Route></Routes></HashRouter>
 }
